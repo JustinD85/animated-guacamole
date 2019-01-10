87824757d41e41fcd50118d60a83633e6e86cef7
Justin <justin@baal-software.com>
2019-01-10 13:45:04 -0700

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       'Our awesome app' 
+      </div>
+    );
+  }
+}
+
+export default App;
