f93ebfcb790171ac8d9931ca497c16072ff1d934
Justin <justin@baal-software.com>
2019-01-11 15:37:58 -0700

    Add Intro Screen

---
+++ src/Components/UserInterface.js
@@ -0,0 +1,19 @@
+import React, { Components } from 'react';
+
+
+class UserInterface extends Components{
+
+  constructor(props) {
+    super(props); //Person, Door
+
+  }
+
+
+  render() {
+  
+
+    return 'Current State of User Interface based on App state';
+  }
+}
+
+export default UserInterface;
