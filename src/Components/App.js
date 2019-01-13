6a3920d45a5be7ce7dc1ba0940c5cb4f38e7c5a3
Justin <justin@baal-software.com>
2019-01-13 09:46:50 -0700

    Check for gamepad

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../Styles/Main.scss';
+import '../styles/Main.scss';
 import Intro from './Intro';
 import Room from './Room';
 import Map from './Map'
@@ -9,15 +9,22 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      playerStatus: 'intro'
+      playerStatus: 'intro',
+      gamePadID: '',
+      actions: {
+        confirm: 0,
+        cancel: 1,
+        info: 3,
+        start: 9
+      }
     }
   }
 
   getPlayerStatus() {
     return {
-      'intro': <Intro render={this.renderArea}/>,
-      'room': <Room render={this.renderArea}/>,
-      'map': <Map render={this.renderArea}/>
+      'intro': <Intro render={this.renderArea} />,
+      'room': <Room render={this.renderArea} />,
+      'map': <Map render={this.renderArea} />
     }[this.state.playerStatus]
   }
 
@@ -25,6 +32,28 @@ class App extends Component {
     this.setState({
       playerStatus: string
     })
+  }
+//Where the magic happens with the controller
+  updateGampad = () => {
+    const gamePad = navigator.getGamepads()[0] || 'No GamePad Connected';
+    const gpButtons = gamePad.buttons;
+    const gpAxes = gamePad.axes;
+    const { actions } = this.state
+
+    console.table(gpButtons);
+  }
+
+  componentWillUnmount() {
+    //Stops the constant checking for controller
+    clearInterval(this.state.gamePadID);
+  }
+
+  componentDidMount() {
+    //Starts the constant check for controller
+    const gamePadID = setInterval(() => {
+      this.updateGampad();
+    }, 500)//slow time for now, dev-only
+    this.setState({ gamePadID });
   }
 
   render() {
